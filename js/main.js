650432ecdc7943306dac63247a3ba46b4b903bdd
Elanis <elanis@hotmail.com>
2020-12-05 19:27:25 +0100

    Import old LUMEN version monitoring module
    With some improvements on main page

--- js/main.js
+++ js/main.js
@@ -1,59 +1,90 @@
 const websites = {};
 
+const websiteStatus = {
+
+};
+
 let updateTimeout = -1;
 
 class Monitoring {
 	static init() {
-		const DOMElt = document.querySelector('.module-monitoring table tbody');
+		if(currentWindow !== 'index') {
+			const DOMElt = document.querySelector('.main-monitoring table tbody');
 
-		for(const website in websites) {
-			const tr = document.createElement('tr');
+			for(const website in websites) {
+				const tr = document.createElement('tr');
 
-			const tdName = document.createElement('td');
-			tdName.innerText = website;
-			tr.append(tdName);
+				const tdName = document.createElement('td');
+				tdName.innerText = website;
+				tr.append(tdName);
 
-			const tdStatus = document.createElement('td');
-			tdStatus.id = 'monitoring-' + website;
-			tdStatus.innerText = '?';
-			tr.append(tdStatus);
+				const tdStatus = document.createElement('td');
+				tdStatus.id = 'monitoring-' + website;
+				tdStatus.innerText = '?';
+				tr.append(tdStatus);
 
-			DOMElt.append(tr);
+				DOMElt.append(tr);
+			}
+		} else {
+			document.getElementById('module-monitoring-cicle-status').addEventListener('click', () => {
+				document.getElementById('monitoring').click();
+			});
 		}
 
 		Monitoring.updateStates();
 	}
 
+	static updateDisplay() {
+		if(currentWindow === 'index') {
+			const statusCircle = document.getElementById('module-monitoring-cicle-status');
+			const statusMessage = document.getElementById('module-monitoring-statusMessage');
+			if(Object.values(websiteStatus).every((e) => e)) {
+				statusCircle.setAttribute('stroke', '#2ECC71');
+				statusMessage.innerText = 'OK';
+			} else {
+				statusCircle.setAttribute('stroke', '#E74C3C');
+				statusMessage.innerText = 'KO';
+			}
+		} else {
+			const DOMElt = document.querySelector('.module-monitoring table tbody');
+
+			for(const website in websiteStatus) {
+				const element = document.getElementById('monitoring-' + website);
+
+				if(websiteStatus[website]) {
+					element.style.color = 'green';
+					element.innerText = 'OK';
+				} else {
+					element.style.color = 'red';
+					element.innerText = 'KO';
+				}
+			}
+		}
+	}
+
 	static updateStates() {
 		for(const website in websites) {
-			const element = document.getElementById('monitoring-' + website);
-
-			Monitoring.writeStatusAsync(websites[website], element);
+			Monitoring.writeStatusAsync(website);
 		}
 
 		Monitoring.launchTimeout();
 	}
 
-	static async writeStatusAsync(url, element) {
-		let error = false;
-		try {
-			const res = await fetch(url);
+	static async writeStatusAsync(website) {
+		websiteStatus[website] = false;
 
-			// console.debug(res);
+		try {
+			const res = await fetch(websites[website]);
 			if(res.status < 200 || res.status > 299) {
-				error = true;
+				websiteStatus[website] = false;
+			} else {
+				websiteStatus[website] = true;
 			}
 		} catch(e) {
-			error = true;
+			websiteStatus[website] = false;
 		}
 
-		if(error) {
-			element.style.color = 'red';
-			element.innerText = 'KO';
-		} else {
-			element.style.color = 'green';
-			element.innerText = 'OK';
-		}
+		Monitoring.updateDisplay();
 	}
 
 	static launchTimeout() {
